9fe148b8b97f80b4123bb011df9b9cc642a7fc14
coconaut <ambarry22@gmail.com>
2016-07-20 23:44:57 -0400

    case typo

--- lib/matchmaker/room.ex
+++ lib/matchmaker/room.ex
@@ -1,4 +1,4 @@
-defmodule MatchMaker.Room do
+defmodule Matchmaker.Room do
   @callback start_link(String.t) :: {:ok, pid}
   @callback join(pid | atom, pid, any) :: {:ok, :joined, any} | :error
   @callback leave(pid, pid) :: :ok
