be966c154082d557c3bb85f918a61581675c0dde
coconaut <ambarry22@gmail.com>
2016-07-20 23:41:25 -0400

    yup

--- lib/matchmaker/room.ex
+++ lib/matchmaker/room.ex
@@ -1,6 +1,6 @@
 defmodule MatchMaker.Room do
   @callback start_link(String.t) :: {:ok, pid}
-  @callback join(pid, pid, any) :: {:ok, :joined, any} | :error
+  @callback join(pid | atom, pid, any) :: {:ok, :joined, any} | :error
   @callback leave(pid, pid) :: :ok
   @callback close(pid) :: :ok
 end
